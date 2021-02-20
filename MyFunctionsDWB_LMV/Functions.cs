f86c1bbfebd0b8e7fefd36f20c1c241545b61be2
Rolando <rolando.fcfm2020@gmail.com>
2021-02-20 16:50:06 -0600

    first commit

---
+++ MyFunctionsDWB_LMV/Functions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFunctionsDWB_LMV
+{
+    public class Functions
+    {
+        public decimal Sum(decimal a, decimal b)
+        {
+
+            return a + b;
+        }
+
+        public decimal Mult(decimal a, decimal b)
+        {
+
+            return a * b;
+        }
+    }
+}
